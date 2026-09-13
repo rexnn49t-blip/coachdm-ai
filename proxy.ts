4f7080b8e0be1a220c116123a9158d09019fe028
Ayush <rexnn49t@gmail.com>
2026-09-13 11:41:00 +0530

    Fix Clerk webhook middleware

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,3 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
-
-export const config = {
-  matcher: [
-    "/((?!_next|.*\\..*|api/webhooks/clerk).*)",
-  ],
-};
